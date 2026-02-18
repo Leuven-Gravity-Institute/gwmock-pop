48ddc00b1ed8c1d2190f04c7fad5f1b22b7636dc
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-02-18 05:35:44 +0100

    feat: Add the planck tapering function

---
+++ src/gwsim_pop/core/distributions/__init__.py
@@ -0,0 +1,7 @@
+"""Core functions for distributions module."""
+
+from __future__ import annotations
+
+from gwsim_pop.core.distributions.smoothing import planck_tapering_function
+
+__all__ = ["planck_tapering_function"]
---
+++ src/gwsim_pop/core/distributions/smoothing.py
@@ -0,0 +1,46 @@
+"""Functions to smooth distributions."""
+
+from __future__ import annotations
+
+import jax
+import jax.numpy as jnp
+
+
+def planck_tapering_function(x: jax.Array, x_min: float, delta: float) -> jax.Array:
+    """Planck tapering function to smoothly transition between 0 and 1.
+
+    Args:
+        x (float): The input value.
+        x_min (float): The minimum value of the transition region.
+        x_max (float): The maximum value of the transition region.
+        delta (float): The width of the transition region.
+
+    Returns:
+        An array with the value of the tapering function at x.
+
+    """
+
+    def planck_rise(x: jax.Array, x_min: float, delta: float) -> jax.Array:
+        """Planck rise function from 0 to 1 over the interval [x_min, x_min + delta].
+
+        Args:
+            x: The input value.
+            x_min: The minimum value of the transition region.
+            delta: The width of the transition region.
+
+        Returns:
+            The value of the rise function at x.
+
+        """
+        a = jnp.exp(delta / (x - x_min) + delta / (x - (x_min + delta)))
+        return 1.0 / (1.0 + a)
+
+    return jnp.where(
+        x < x_min,
+        0.0,
+        jnp.where(
+            x < x_min + delta,
+            planck_rise(x, x_min, delta),
+            1.0,
+        ),
+    )
