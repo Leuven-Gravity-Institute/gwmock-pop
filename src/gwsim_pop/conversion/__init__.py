f13d27c1eb4a664ce42549d911e44d81f1084cea
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-02-26 13:12:10 +0100

    feat: Add a function to compute the symmetric mass ratio from component masses

--- src/gwsim_pop/conversion/__init__.py
+++ src/gwsim_pop/conversion/__init__.py
@@ -2,6 +2,9 @@
 
 from __future__ import annotations
 
-from gwsim_pop.conversion.cbc import compute_chirp_mass_from_mass_1_mass_2
+from gwsim_pop.conversion.cbc import (
+    compute_chirp_mass_from_mass_1_mass_2,
+    compute_symmetric_mass_ratio_from_mass_1_mass_2,
+)
 
-__all__ = ["compute_chirp_mass_from_mass_1_mass_2"]
+__all__ = ["compute_chirp_mass_from_mass_1_mass_2", "compute_symmetric_mass_ratio_from_mass_1_mass_2"]
--- src/gwsim_pop/conversion/cbc.py
+++ src/gwsim_pop/conversion/cbc.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import jax.numpy as jnp
 from jax import Array
 
 
@@ -16,3 +17,16 @@ def compute_chirp_mass_from_mass_1_mass_2(mass_1: Array, mass_2: Array) -> Array
         The chirp mass of the binary (in solar masses).
     """
     return (mass_1 * mass_2) ** 0.6 / (mass_1 + mass_2) ** 0.2
+
+
+def compute_symmetric_mass_ratio_from_mass_1_mass_2(mass_1: Array, mass_2: Array) -> Array:
+    """Compute the symmetric mass ratio from the component masses of the binary.
+
+    Args:
+        mass_1: The mass of the first component object in the binary (in solar masses).
+        mass_2: The mass of the second component object in the binary (in solar masses).
+
+    Returns:
+        The symmetric mass ratio of the binary.
+    """
+    return jnp.minimum((mass_1 * mass_2) / (mass_1 + mass_2) ** 2, 0.25)
