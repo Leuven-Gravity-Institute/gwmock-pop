309faaa424a6ff6ced7b67a8515b1bb9d9c51b2d
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-02-26 20:49:57 +0100

    chore: Remove simulators from api

--- src/gwsim_pop/api/simulators/__init__.py
+++
@@ -1,8 +0,0 @@
-"""API for the simulator classes."""
-
-from __future__ import annotations
-
-from gwsim_pop.simulators.factory import get_simulator
-from gwsim_pop.simulators.simulator import Simulator
-
-__all__ = ["Simulator", "get_simulator"]
