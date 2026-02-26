59254f4c955651c5a25a17d1548d45fb20bf559d
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-02-26 13:32:39 +0100

    feat: Add a function to compute the mass ratio from the component masses

--- src/gwsim_pop/conversion/__init__.py
+++ src/gwsim_pop/conversion/__init__.py
@@ -4,12 +4,14 @@ from __future__ import annotations
 
 from gwsim_pop.conversion.cbc import (
     compute_chirp_mass_from_mass_1_mass_2,
+    compute_mass_ratio_from_mass_1_mass_2,
     compute_symmetric_mass_ratio_from_mass_1_mass_2,
     compute_total_mass_from_mass_1_mass_2,
 )
 
 __all__ = [
     "compute_chirp_mass_from_mass_1_mass_2",
+    "compute_mass_ratio_from_mass_1_mass_2",
     "compute_symmetric_mass_ratio_from_mass_1_mass_2",
     "compute_total_mass_from_mass_1_mass_2",
 ]
--- src/gwsim_pop/conversion/cbc.py
+++ src/gwsim_pop/conversion/cbc.py
@@ -43,3 +43,17 @@ def compute_total_mass_from_mass_1_mass_2(mass_1: Array, mass_2: Array) -> Array
         The total mass of the binary (in solar masses).
     """
     return mass_1 + mass_2
+
+
+def compute_mass_ratio_from_mass_1_mass_2(mass_1: Array, mass_2: Array) -> Array:
+    """Compute the mass ratio from the component masses of the binary.
+
+    Args:
+        mass_1: The mass of the first component object in the binary (in solar masses).
+        mass_2: The mass of the second component object in the binary (in solar masses).
+
+    Returns:
+        The mass ratio, m2/m1, where m2 <= m1.
+    """
+    return mass_2 / mass_1
+    return mass_2 / mass_1
