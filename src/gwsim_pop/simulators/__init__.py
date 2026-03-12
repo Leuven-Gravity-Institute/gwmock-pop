270107f631d316e95be7610aa9ba41e6b268db7e
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-03-12 16:17:25 +0100

    chore: Add `GraphSimulator` to the public API

--- src/gwsim_pop/simulators/__init__.py
+++ src/gwsim_pop/simulators/__init__.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from gwsim_pop.simulators.graph import GraphSimulator
 from gwsim_pop.simulators.simulator import Simulator
 
-__all__ = ["Simulator"]
+__all__ = ["GraphSimulator", "Simulator"]
