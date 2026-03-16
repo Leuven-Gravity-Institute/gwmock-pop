3df5a0bf3ecc9f27e60af0c50fd091964b97f245
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-03-16 16:52:40 +0100

    chore: Remove transforms temporarily

--- src/gwsim_pop/transforms/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Modules to perform transformations."""
-
-from __future__ import annotations
-
-from gwsim_pop.transforms.event_rate import compute_merger_event_rate
-
-__all__ = ["compute_merger_event_rate"]
