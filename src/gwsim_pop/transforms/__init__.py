cf5b356a7665ce4e29d5bca9e3a8d2b670f326ec
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-03-16 16:32:18 +0100

    fix: Remove non-existent functions

--- src/gwsim_pop/transforms/__init__.py
+++ src/gwsim_pop/transforms/__init__.py
@@ -3,6 +3,5 @@
 from __future__ import annotations
 
 from gwsim_pop.transforms.event_rate import compute_merger_event_rate
-from gwsim_pop.transforms.n_samples import get_n_samples
 
-__all__ = ["compute_merger_event_rate", "get_n_samples"]
+__all__ = ["compute_merger_event_rate"]
